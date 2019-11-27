f0ac2d7528ae1fd419ca162c4cedb974b6bcd8b4
Minh Truong <congminh.truong.ext@parrot.com>
2019-11-27 14:31:23 +0100

    [DEV] Update dependencies and add navigation

--- app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/Meal.kt
+++ app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/Meal.kt
@@ -1,2 +1,31 @@
 package com.eleven.ctruong.w2eat.repositories.models
 
+data class Meal(
+    var id: String,
+    var photosUrl: MutableList<String>,
+    var name: String,
+    var category: Category,
+    var recipe: Recipe,
+    var reviews: MutableList<Review>
+)
+
+enum class Category {
+    ASIAN, FRENCH, ITALIAN
+}
+
+enum class Language {
+    EN, FR, VN
+}
+
+data class Recipe(
+    var id: String,
+    var description: String,
+    var steps: MutableList<Step>,
+    var languageSupport: String
+)
+
+data class Step(
+    var stepNumber: Int,
+    var name: String,
+    var description: String
+)
--- app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/User.kt
+++ app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/User.kt
@@ -1,2 +1,18 @@
 package com.eleven.ctruong.w2eat.repositories.models
 
+data class User(
+    var email: String,
+    var password: String,
+    var meal: MutableList<Meal>,
+    var favoriteMeals: MutableList<Meal>,
+    var reviews: MutableList<Review>
+)
+
+data class Review(
+    var id: String,
+    var author: User,
+    var meal: Meal,
+    var note: Int,
+    var comment: String,
+    var date: String
+)
