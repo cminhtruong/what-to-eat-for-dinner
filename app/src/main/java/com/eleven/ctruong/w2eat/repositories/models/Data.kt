44e2ff0571fb247aabee672f95484e160753fa8d
Minh Truong <congminh.truong.ext@parrot.com>
2019-12-10 16:40:01 +0100

    [DEV] Fix build #10 error.
    Todo: - Implement design to code in order to created login module.
          - Link to Firebase Auth and Firestore

--- app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/Data.kt
+++ app/src/main/java/com/eleven/ctruong/w2eat/repositories/models/Data.kt
@@ -11,31 +11,31 @@ import androidx.room.*
  */
 @Entity
 data class User(
-    @PrimaryKey var email: String,
-    var password: String? = ""
+    @PrimaryKey val email: String,
+    val password: String? = ""
 )
 
 @Entity
 data class Review(
     @PrimaryKey
-    var reviewId: String,
-    var author: String,
-    var recipe: String? = "",
-    var note: Int? = 0,
-    var comment: String? = "",
-    var date: String? = ""
+    val reviewId: String,
+    val author: String,
+    val recipe: String? = "",
+    val note: Int? = 0,
+    val comment: String? = "",
+    val date: String? = ""
 )
 
 @Entity
 data class Meal(
     @PrimaryKey
-    var mealId: String,
-    var name: String? = "",
-    var owner: String,
+    val mealId: String,
+    val name: String? = "",
+    val owner: String,
     @Ignore
-    var photosUrl: MutableList<String>? = mutableListOf(),
+    val photosUrl: MutableList<String>? = mutableListOf(),
     @Ignore
-    var category: Category? = Category.FRENCH
+    val category: Category? = Category.FRENCH
 )
 
 enum class Category {
@@ -48,22 +48,22 @@ enum class Language {
 
 @Entity
 data class Recipe(
-    @PrimaryKey var recipeId: String,
-    var author: String,
-    var recipeMealId: String,
+    @PrimaryKey val recipeId: String,
+    val author: String,
+    val recipeMealId: String,
     @Ignore
-    var languageSupport: Language? = Language.EN,
+    val languageSupport: Language? = Language.EN,
     @ColumnInfo(name = "note")
-    var noteAverage: Float? = 0.0f
+    val noteAverage: Float? = 0.0f
 )
 
 @Entity
 data class Step(
-    @PrimaryKey var stepId: String,
-    var stepRecipeId: String,
-    var stepNumber: Int? = 0,
-    var name: String? = "",
-    var description: String? = ""
+    @PrimaryKey val stepId: String,
+    val stepRecipeId: String,
+    val stepNumber: Int? = 0,
+    val name: String? = "",
+    val description: String? = ""
 )
 
 @Entity
@@ -71,64 +71,63 @@ data class Favorite(
     @PrimaryKey(autoGenerate = true)
     val id: Long? = 0,
     val userId: String? = "",
-    @Embedded
-    val meals: Meal
+    val meals: MutableList<Meal>
 )
 
 data class UserWithFavorites(
     @Embedded
-    var user: User,
+    val user: User,
     @Relation(
         parentColumn = "email",
         entityColumn = "userId"
     )
-    var favorite: Favorite
+    val favorite: Favorite
 )
 
 data class MealAndRecipe(
-    @Embedded var meal: Meal,
+    @Embedded val meal: Meal,
     @Relation(
         parentColumn = "mealId",
         entityColumn = "recipeMealId"
     )
-    var recipe: Recipe
+    val recipe: Recipe
 )
 
 
 data class RecipeWithSteps(
     @Embedded
-    var recipe: Recipe,
+    val recipe: Recipe,
     @Relation(
         parentColumn = "recipeId",
         entityColumn = "stepRecipeId"
     )
-    var steps: MutableList<Step>? = mutableListOf()
+    val steps: MutableList<Step>? = mutableListOf()
 )
 
 data class RecipeAndReviews(
     @Embedded
-    var recipe: Recipe,
+    val recipe: Recipe,
     @Relation(
         parentColumn = "recipeId",
         entityColumn = "recipe"
     )
-    var reviews: MutableList<Review>? = mutableListOf()
+    val reviews: MutableList<Review>? = mutableListOf()
 )
 
 data class UserWithOwnMeals(
-    @Embedded var user: User,
+    @Embedded val user: User,
     @Relation(
         parentColumn = "email",
         entityColumn = "owner"
     )
-    var meals: MutableList<Meal>? = mutableListOf()
+    val meals: MutableList<Meal>? = mutableListOf()
 )
 
 data class UserAndReviews(
-    @Embedded var user: User,
+    @Embedded val user: User,
     @Relation(
         parentColumn = "email",
         entityColumn = "author"
     )
-    var reviews: MutableList<Review>? = mutableListOf()
+    val reviews: MutableList<Review>? = mutableListOf()
 )
