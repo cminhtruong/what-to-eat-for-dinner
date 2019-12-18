4ede9361a6a47dc203ac9e8b92eb8abfc5d08253
ctruong <congminh.truong.ext@parrot.com>
2019-12-18 11:21:43 +0100

    [DEV] Update new component in androidx TextInputLayout and add gitignore
    Todo: - Implement design to code in order to created login module.
          - Link to Firebase Auth and Firestore

--- app/src/main/java/com/eleven/ctruong/w2eat/auth/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/eleven/ctruong/w2eat/auth/ui/signup/SignUpViewModel.kt
@@ -1,6 +1,7 @@
 package com.eleven.ctruong.w2eat.auth.ui.signup
 
 import androidx.lifecycle.ViewModel
+import com.eleven.ctruong.w2eat.repositories.local.AppDatabaseDao
 
 /**
  * ViewModel for sign up fragment
@@ -9,5 +10,9 @@ import androidx.lifecycle.ViewModel
  * @version 1.0
  * @since 2019, Dec 2nd
  */
-class SignUpViewModel: ViewModel() {
+class SignUpViewModel(
+    private val email: String,
+    private val password: String,
+    val database: AppDatabaseDao
+) : ViewModel() {
 }
